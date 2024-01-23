63364538a78907f76e633475c643b2091cebcd97
edwardvish <ebv2489@gmail.com>
2024-01-23 12:39:50 +0200

    Configure Conftest file and setup various web drivers

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 driver = None
-browser = 'Chrome'
+browser = 'Edge'
 
 
 @pytest.fixture(scope='class')
